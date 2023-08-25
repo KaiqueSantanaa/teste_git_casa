3fa2436a5274ead34667cae1056aff0b7c034e1f
Kaique Santana <kaiquesantanacontato@gmail.com>
2023-08-24 23:30:27 -0300

    ap01(git): divisao implementada

--- Calculadora.py
+++ Calculadora.py
@@ -2,8 +2,11 @@ def soma (a,b):
     return a+b
 
 def subtracao (a,b):
-    #return soma(a,-b)
+    
     return a-b
 
 def multiplicacao (a,b):
     return a*b
+
+def divisao (a, b):
+    return a/b
